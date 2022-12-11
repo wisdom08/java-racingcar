e395ca58cda5d1e6226743096394059b4e3a5270
wisdom08 <wisdom08@kakao.com>
2022-12-12 00:33:29 +0900

    fix: 하나의 케이스인 경우에 test 로 변경함

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,14 +17,14 @@ public class StringTest {
         assertThat(split).isEqualTo(new String[]{"1", "2"});
     }
 
-    @ParameterizedTest
+    @Test
     @ValueSource(strings = "1")
     void split_one(String input) {
         String[] split = input.split(",");
         assertThat(split).containsExactly("1");
     }
 
-    @ParameterizedTest
+    @Test
     @ValueSource(strings = "(1,2)")
     void substringTest(String input) {
         String substring = input.substring(1).substring(0, input.length()-2);
